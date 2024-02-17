e97bf78ecb6932f858bab045d0e8576aa108a12a
Harrison Verrios <harrison.v1@live.com>
2024-02-17 19:01:59 +1100

    Updated the package script.

--- Package.swift
+++ Package.swift
@@ -5,16 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "JsonKit",
+    products: [
+        .library(
+            name: "JsonKit",
+            targets: ["JsonKit"]
+        ),
+    ],
     dependencies: [
-        .package(url: "https://github.com/harrisonv789/JsonKit.git", from: "0.1.0"),
+        // Add any dependencies here if needed
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JsonKit"),
+            name: "JsonKit",
+            dependencies: []
+        ),
         .testTarget(
             name: "JsonKitTests",
-            dependencies: ["JsonKit"]),
+            dependencies: ["JsonKit"]
+        ),
     ]
 )
