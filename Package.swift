3f21b3f00913fa1f7dec6a7321debe702d23acfc
Harrison Verrios <harrison.v1@live.com>
2024-02-17 18:59:18 +1100

    Updated the version for the initial tag of the package.

--- Package.swift
+++ Package.swift
@@ -5,11 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "JsonKit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "JsonKit",
-            targets: ["JsonKit"]),
+    dependencies: [
+        .package(url: "https://github.com/harrisonv789/JsonKit.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
