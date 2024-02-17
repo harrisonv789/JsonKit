a1c2a5b40b790f26e614897d8ed56cfc3af7add5
Harrison Verrios <harrison.v1@live.com>
2024-02-17 18:55:10 +1100

    Updated the comment

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Created by: Harrison Verrios, 2024
 
 import PackageDescription
 
